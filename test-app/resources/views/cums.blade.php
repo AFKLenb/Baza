12d488eebb8d700cb13c4d375b721023ba3a1acd
AFKLenb <markermma@gmail.com>
2024-02-01 18:37:46 +0300

    сделали категории и товары как в dns

--- test-app/resources/views/cums.blade.php
+++ test-app/resources/views/cums.blade.php
@@ -11,8 +11,8 @@
                         <h5 style="padding-bottom: 20px; font-size: 20px; height: 100px;" class="card-title">{{$cum->name}}</h5>
                         <p style="font-size: 16px; height: 150px; overflow-y: scroll; " class="card-text">{{$cum->text}}</p>
                         <div style="margin-top: 40px; margin-bottom: 20px; display: flex; justify-content:space-between;" class="card__buttons">
-                            <p style="font-size: 20px; text-transform: uppercase; " class="card-text">{{$cum->price}}</p>
-                            <p style="font-size: 20px;; " class="card-text">Колличество: {{$cum->quantity}}</p>
+                            <p style="font-size: 24px; text-transform: uppercase; " class="card-text">{{$cum->price}}</p>
+                            <p style="font-size: 24px;; " class="card-text">Колличество: {{$cum->quantity}}</p>
                         </div>
                         <td>
                             @if($cum->rating == 0)
--- test-app/resources/views/single-cum.blade.php
+++ test-app/resources/views/single-cum.blade.php
@@ -5,10 +5,10 @@
         <h2>{{$cum->name}}</h2>
         <div style="display: flex; align-items: start; gap: 60px; margin: 50px 0; padding: 20px; background-color: #2c2c2c; border-radius: 25px" class="card__content">
             <div class="">
-                <img style="border-radius: 15px" width="250" height="250" src="/images/cums/{{$cum->image}}" alt="">
+                <img style="border-radius: 15px" width="350" height="300" src="/images/cums/{{$cum->image}}" alt="">
                 <div style="display: flex; justify-content:space-between; margin-top: 20px;" class="">
-                    <p style="font-size: 20px; text-transform: uppercase; " class="card-text">{{$cum->price}}</p>
-                    <p style="font-size: 20px;; " class="card-text">Колличество: {{$cum->quantity}}</p>
+                    <p style="font-size: 24px; text-transform: uppercase; " class="card-text">{{$cum->price}}</p>
+                    <p style="font-size: 24px;; " class="card-text">Колличество: {{$cum->quantity}}</p>
                 </div>
                 @if($cum->rating == 0)
                     <div class="alert alert-danger text-center">
@@ -33,8 +33,8 @@
                 @endif
             </div>
             <div class="">
-                <h3 style="font-size: 20px; text-transform: uppercase; font-weight: 700;">Описание:</h3>
-                <p>{{$cum->text}}</p>
+                <h3 style="font-size: 24px; text-transform: uppercase; font-weight: 700;">Описание:</h3>
+                <p style="font-size: 18px; margin-top: 20px;">{{$cum->text}}</p>
             </div>
 
         </div>
